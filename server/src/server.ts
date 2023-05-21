17320dad92df0fd2eb5946cc87cde4d62431526a
Philipp Moreira <psmdeveloper@gmail.com.br>
2023-05-21 13:34:32 -0300

    feat ( Server ) Adcionado CRUD Memórias.
    Issue #Aula2

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,15 +1,19 @@
-import { PrismaClient } from '@prisma/client'
+import cors from '@fastify/cors'
 import fastify from 'fastify'
 
-const porta = 3333
-const app = fastify()
-const repository = new PrismaClient()
+import { memoriesRoutes } from './routes/memories'
 
-app.get('/users', async () => {
-  const users = await repository.user.findMany()
-  return users
+const _port = 3333
+const app = fastify()
+
+app.register(cors, {
+  // Aceita todas as requesições independente da origem
+  origin: true,
+  // Abaixo exemplo de como seria especificando URLs especificas que poderiam acessar o este backend.
+  // origin: ['http://localhost:2000', 'http://localhost:4000', 'http://localhost:5000'],
 })
+app.register(memoriesRoutes)
 
 app
-  .listen({ port: porta })
-  .then(() => console.info(`Server HTTP is running on ${porta}`))
+  .listen({ port: _port })
+  .then(() => console.info(`Server HTTP is running on ${_port}`))
